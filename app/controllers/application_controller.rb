f8bdda79871c92925016601ca8e1b925f276c650
Etienne Depaulis <etienne.depaulis@gmail.com>
2018-05-04 20:52:40 +0200

    Correctly configured signup process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    user_params.permit(:email, :password, :password_confirmation, :policy_rule_age, :policy_rule_privacy_terms)
+  end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  include PolicyManager::Concerns::UserBehavior
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
