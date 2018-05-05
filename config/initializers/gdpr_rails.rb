7c4f745935a59de637dc6a7de8a11268b889f0a0
Etienne Depaulis <etienne.depaulis@gmail.com>
2018-05-05 08:32:24 +0200

    Cleaned gdpr_rails initializers

--- config/initializers/gdpr_rails.rb
+++ config/initializers/gdpr_rails.rb
@@ -1,20 +1,4 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
-
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-
-config = PolicyManager::Config.setup do |c|
+PolicyManager::Config.setup do |c|
   c.logout_url = "logout"
   c.from_email = "admin@acme.com"
   c.admin_email_inbox = "foo@baaz.org"
