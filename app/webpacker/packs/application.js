4e74c6783344dd89b63bae1e80d81839882d32ad
Etienne Depaulis <etienne.depaulis@gmail.com>
2018-05-08 17:04:07 +0200

    Configured cookieconsent and webpacker

---
+++ app/webpacker/packs/application.js
@@ -0,0 +1,12 @@
+/* eslint no-console:0 */
+
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+
+import { CookieConsent } from 'cookieconsent';
+
+import "./application.scss"
+
+Rails.start();
+Turbolinks.start();
+
